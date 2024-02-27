e7bd6e1c071d61c88bfe86417763d8aa6f033e90
ks <komalsai130@gmail.com>
2024-02-27 19:29:18 +0530

    updated readme

--- READme.md
+++ READme.md
@@ -1,3 +1,22 @@
 ## Real time progress of celery task using django channels
 
 ` celery -A server  worker -l info`
+
+### Set up
+
+```
+git clone https://github.com/IMinevitable-3/background-progress.git &&
+
+cd background-progress &&
+virtualenv env &&
+source env/bin/activate &&
+cd server   &&
+pip install -r requirements.txt &&
+python manage.py runserver
+```
+
+```
+docker-compose build
+docker-compose up
+
+```
--- server/environ.py
+++ server/environ.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DJANGO_DEBUG = os.environ.get("DJANGO_DEBUG", True)
-SECRET_KEY = os.environ.get("SECRET_KEY", None)
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-82t$7=d*8)y!d!x-^qupjm%!9rm1o5z7rc2#6040dfhk!xy+)0"
+)
 RABBIT_USER = os.environ.get("RABBIT_USER", "user")
 RABBIT_PASSWORD = os.environ.get("RABBIT_PASSWORD", "password")
 RABBIT_HOST = os.environ.get("RABBIT_HOST", "localhost")
